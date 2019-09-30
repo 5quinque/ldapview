1b89e7eed547b4740892b2d306111b1cb6b3989d
Ryan Linnit <ryan@ryanl.co.uk>
2019-09-30 17:47:13 +0100

    Added homepage and list hosts in netgroup page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\HostRepository;
+use App\Repository\NetgroupRepository;
+use App\Repository\PeopleRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(HostRepository $hostRepository, NetgroupRepository $netgroupRepository, PeopleRepository $peopleRepository)
     {
+        $host_query = $hostRepository->createQueryBuilder('p')->getQuery();
+        $host_paginator = new Paginator($host_query, $fetchJoinCollection = true);
+        $host_count = $host_paginator->count();
+
+        $netgroup_query = $netgroupRepository->createQueryBuilder('p')->getQuery();
+        $netgroup_paginator = new Paginator($netgroup_query, $fetchJoinCollection = true);
+        $netgroup_count = $netgroup_paginator->count();
+
+        $people_query = $peopleRepository->createQueryBuilder('p')->getQuery();
+        $people_paginator = new Paginator($people_query, $fetchJoinCollection = true);
+        $people_count = $people_paginator->count();
+
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'host_count' => $host_count,
+            'netgroup_count' => $netgroup_count,
+            'people_count' => $people_count,
         ]);
     }
 }
