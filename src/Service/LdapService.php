918837c1767556582ab8ff45de86281750e86839
linnit <ryan@ryanl.co.uk>
2019-10-03 14:04:38 +0100

    using ldap to get users

---
+++ src/Service/LdapService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Ldap\Ldap;
+
+class LdapService
+{
+    private $ldap;
+
+    public function __construct()
+    {
+        $this->ldap = Ldap::create('ext_ldap', [
+            'host' => $_ENV['LDAP_HOST'],
+            'encryption' => 'none',
+        ]);
+
+        $this->ldap->bind($_ENV['LDAP_USER'], $_ENV['LDAP_PASS']);
+    }
+
+    public function findOneByUid(string $uid): object
+    {
+        $query = $this->ldap->query('dc=example,dc=org',
+            "(&(ObjectClass=posixAccount)(uid={$uid}))",
+            ["maxItems" => 1]
+        );
+
+        return $query->execute()->toArray()[0];
+    }
+}
